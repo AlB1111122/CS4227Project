43d19066e5efd4990a24aab330278b1291fa4f43
Alex <21338787@studentmail.ul.ie>
2025-02-26 21:22:14 +0000

    model update

--- project_service/project_service/models.py
+++ project_service/project_service/models.py
@@ -6,9 +6,6 @@ class Timeline(models.Model):
     timeline_start = models.DateField()
     timeline_end = models.DateField()
 
-    def __str__(self):
-        return f"Timeline {self.id}"
-
 class Project(models.Model):
     id = models.AutoField(primary_key=True)
     created_at = models.DateField(auto_now_add=True)
@@ -16,15 +13,9 @@ class Project(models.Model):
     name = models.CharField(max_length=255)
     description = models.CharField(max_length=600)
 
-    def __str__(self):
-        return f"Project {self.id}"
-
 class EventType(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255, unique=True)
-
-    def __str__(self):
-        return self.name
 
 class Event(models.Model):
     id = models.AutoField(primary_key=True)
@@ -36,15 +27,15 @@ class Event(models.Model):
     description = models.CharField(max_length=600)
     type = models.ForeignKey(EventType, on_delete=models.CASCADE)
     financial_impact= models.IntegerField()
-
-    def __str__(self):
-        return f"Event {self.name} ({self.id})"
+    document_id = models.IntegerField(blank=True, null=True)
 
 class ProjectMember(models.Model):
+    class Role(models.TextChoices):
+        OWNER = 'OWNER'
+        EDITOR = 'EDITOR'
+        VIEWER = 'VIEWER'
+
     id = models.AutoField(primary_key=True)
     project_id = models.ForeignKey(Project, on_delete=models.CASCADE, db_index=True)
     user_id = models.IntegerField()#not unique
-    role = models.CharField(max_length=255)
-
-    def __str__(self):
-        return f"Member {self.user_id} in Project {self.project.id}"
+    role = models.CharField(max_length=255,choices=Role.choices,default=Role.VIEWER, blank=True)
